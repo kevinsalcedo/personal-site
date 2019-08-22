ece0ffae8c677ed0105e397d1b68992d598fb4e2
Kevin <kevsalcedo@gmail.com>
2019-08-22 15:56:09 -0500

    replaced trails divs stylings and divs in Home with Grid

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
+import { Grid } from 'semantic-ui-react'
 import { useTrail, animated, useSpring } from "react-spring";
 
-const items = ["Hi there,", "I'm Kevin Salcedo."];
+const items = [ "Hi there,", "I'm Kevin Salcedo." ];
 const config = { mass: 5, tension: 2000, friction: 400 };
 
 const subheading = "Software Engineer, Rock Climber, Dog Dad";
 
 const Home = () => {
-  const [toggle] = useState(true);
+  const [ toggle ] = useState(true);
   const trail = useTrail(items.length, {
     config,
     opacity: toggle ? 1 : 0,
@@ -25,28 +26,30 @@ const Home = () => {
       friction: 30
     }
   });
-
   return (
-    <div className="trails-main">
-      <div>
-        {trail.map(({ x, height, ...rest }, index) => (
-          <animated.div
-            key={items[index]}
-            className="trails-text"
-            style={{
-              ...rest,
-              transform: x.interpolate(x => `translate3d(0,${x}px,0)`)
-            }}
-          >
-            <animated.div style={{ height }}>{items[index]}</animated.div>
-          </animated.div>
-        ))}
-        <animated.div style={props} className="trails-sub">
-          <h3>{subheading}</h3>
-        </animated.div>
-      </div>
-    </div>
-  );
+    <Grid columns='equal' rows='equal' padded className="parallax-grid">
+      <Grid.Row />
+      <Grid.Row>
+        <Grid.Column />
+        <Grid.Column width={ 14 }>
+          { trail.map(({ x, height, ...rest }, index) => (
+            <animated.div
+              key={ items[ index ] }
+              className="trails-text"
+              style={ {
+                ...rest,
+                transform: x.interpolate(x => `translate3d(0,${x}px,0)`)
+              } }
+            >
+              <animated.div style={ { height } }>{ items[ index ] }</animated.div>
+            </animated.div>
+          )) }
+        </Grid.Column>
+        <Grid.Column />
+      </Grid.Row>
+      <Grid.Row></Grid.Row>
+    </Grid >
+  )
 };
 
 export default Home;
