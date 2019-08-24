c0bae1a96d3a895ee5608c87090fb234ae0305df
Kevin Salcedo <kevinsalcedo@gmail.com>
2019-08-24 12:19:34 -0500

    some sizing changes for responsive layout

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import { Grid } from 'semantic-ui-react'
+import { Grid } from "semantic-ui-react";
 import { useTrail, animated, useSpring } from "react-spring";
 
-const items = [ "Hi there,", "I'm Kevin Salcedo." ];
+const items = ["Hi there,", "I'm Kevin Salcedo."];
 const config = { mass: 5, tension: 2000, friction: 400 };
 
 const subheading = "Software Engineer, Rock Climber, Dog Dad";
 
 const Home = () => {
-  const [ toggle ] = useState(true);
+  const [toggle] = useState(true);
   const trail = useTrail(items.length, {
     config,
     opacity: toggle ? 1 : 0,
@@ -27,29 +27,36 @@ const Home = () => {
     }
   });
   return (
-    <Grid columns='equal' rows='equal' padded className="parallax-grid">
+    <Grid columns="equal" padded className="parallax-grid">
+      <Grid.Row />
       <Grid.Row />
       <Grid.Row>
         <Grid.Column />
-        <Grid.Column width={ 14 }>
-          { trail.map(({ x, height, ...rest }, index) => (
+        <Grid.Column width={15}>
+          {trail.map(({ x, height, ...rest }, index) => (
             <animated.div
-              key={ items[ index ] }
+              key={items[index]}
               className="trails-text"
-              style={ {
+              style={{
                 ...rest,
                 transform: x.interpolate(x => `translate3d(0,${x}px,0)`)
-              } }
+              }}
             >
-              <animated.div style={ { height } }>{ items[ index ] }</animated.div>
+              <animated.div style={{ height }}>{items[index]}</animated.div>
             </animated.div>
-          )) }
+          ))}
         </Grid.Column>
         <Grid.Column />
+        <Grid.Column width={15}>
+          <animated.div style={props}>
+            <h3 className="trails-sub">{subheading}</h3>
+          </animated.div>
+        </Grid.Column>
       </Grid.Row>
-      <Grid.Row></Grid.Row>
-    </Grid >
-  )
+      <Grid.Row />
+      <Grid.Row />
+    </Grid>
+  );
 };
 
 export default Home;
