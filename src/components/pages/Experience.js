3291a86b2a6270c1673c8c74f32f39a147db92a6
Kevin <kevsalcedo@gmail.com>
2019-09-03 12:09:26 -0500

    replaced semantic Grid with React Bootstrap Container

--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { Grid, Menu, List } from "semantic-ui-react";
+import { Menu, List } from "semantic-ui-react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Transition } from "react-spring/renderprops";
 import { jobs } from "../data/work";
 
@@ -12,72 +15,80 @@ class Experience extends React.Component {
     this.setState({ index });
   };
 
-  render() {
+  renderSelect = (index) => {
+    return (
+      <Menu secondary vertical compact>
+        { jobs.map(({ name }, jobIndex) => {
+          return (
+            <Menu.Item
+              name={ name }
+              key={ name }
+              active={ index === jobIndex }
+              onClick={ () => this.toggleWork(jobIndex) }
+            >
+              { name }
+            </Menu.Item>
+          );
+        }) }
+      </Menu>
+
+
+    )
+  }
+
+  renderDescription = (index) => {
+    return (<Transition
+      items={ index }
+      from={ {
+        position: "absolute",
+        transform: "translate3d(0,-150px,0)",
+        opacity: 0
+      } }
+      enter={ { transform: "translate3d(0,0px,0)", opacity: 1 } }
+      leave={ { transform: "translate3d(0,150px,0)", opacity: 0 } }
+      config={ {
+        mass: 10,
+        friction: 75
+      } }
+    >
+      { newIndex => props => {
+        const newJob = jobs[ newIndex ];
+        return (
+          <div style={ props }>
+            <h3>{ newJob.title }</h3>
+            <List>
+              { newJob.description.map((item, i) => (
+                <List.Item key={ i }>{ item }</List.Item>
+              )) }
+            </List>
+          </div>
+        );
+      } }
+    </Transition>
+    )
+
+  }
+
+  renderGrid = () => {
     const { index } = this.state;
     return (
-      <Grid
-        columns="equal"
-        rows="equal"
-        padded
-        relaxed="very"
-        stackable
-        className="parallax-grid"
-      >
-        <Grid.Row />
-        <Grid.Row only="large screen" />
-        <Grid.Row columns="equal">
-          <Grid.Column />
-          <Grid.Column width={4}>
-            <Menu secondary vertical compact>
-              {jobs.map(({ name }, jobIndex) => {
-                return (
-                  <Menu.Item
-                    name={name}
-                    key={name}
-                    active={index === jobIndex}
-                    onClick={() => this.toggleWork(jobIndex)}
-                  >
-                    {name}
-                  </Menu.Item>
-                );
-              })}
-            </Menu>
-          </Grid.Column>
-          <Grid.Column width={8}>
-            <Transition
-              items={index}
-              from={{
-                position: "absolute",
-                transform: "translate3d(0,-150px,0)",
-                opacity: 0
-              }}
-              enter={{ transform: "translate3d(0,0px,0)", opacity: 1 }}
-              leave={{ transform: "translate3d(0,150px,0)", opacity: 0 }}
-              config={{
-                mass: 10,
-                friction: 75
-              }}
-            >
-              {newIndex => props => {
-                const newJob = jobs[newIndex];
-                return (
-                  <div style={props}>
-                    <h3>{newJob.title}</h3>
-                    <List>
-                      {newJob.description.map((item, i) => (
-                        <List.Item key={i}>{item}</List.Item>
-                      ))}
-                    </List>
-                  </div>
-                );
-              }}
-            </Transition>
-          </Grid.Column>
-          <Grid.Column />
-        </Grid.Row>
-        <Grid.Row />
-      </Grid>
-    );
+      <Container className="" >
+        <Row >
+          <Col xs={ 12 } md={ 2 } />
+          <Col xs={ 12 } md={ 4 } >
+            { this.renderSelect(index) }
+          </Col>
+          <Col xs={ 12 } md={ 4 } >
+            { this.renderDescription(index) }
+          </Col>
+          <Col xs={ 12 } md={ 2 } />
+        </Row>
+      </Container >
+    )
+  }
+
+  render() {
+    return this.renderGrid();
   }
 }
 export default Experience;
