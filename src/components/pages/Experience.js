c89522412327935700e0635b1e9f4caee5d83b9f
Kevin Salcedo <kevinsalcedo@gmail.com>
2019-09-05 16:31:32 -0500

    replaced all semantic components with react-bootstrap components, added styling to lists

--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Menu, List } from "semantic-ui-react";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { Transition } from "react-spring/renderprops";
 import { jobs } from "../data/work";
 
@@ -15,75 +16,79 @@ class Experience extends React.Component {
     this.setState({ index });
   };
 
-  renderSelect = (index) => {
+  renderSelect = index => {
     return (
-      <Menu secondary vertical compact>
-        { jobs.map(({ name }, jobIndex) => {
-          return (
-            <Menu.Item
-              name={ name }
-              key={ name }
-              active={ index === jobIndex }
-              onClick={ () => this.toggleWork(jobIndex) }
-            >
-              { name }
-            </Menu.Item>
-          );
-        }) }
-      </Menu>
+      <ListGroup variant="flush">
+        {jobs.map(({ name }, jobIndex) => (
+          <ListGroupItem
+            name={name}
+            key={name}
+            active={index === jobIndex}
+            onClick={() => this.toggleWork(jobIndex)}
+            className="trails-menu trails-menu-hover"
+            action
+          >
+            {name}
+          </ListGroupItem>
+        ))}
+      </ListGroup>
+    );
+  };
 
-
-    )
-  }
-
-  renderDescription = (index) => {
-    return (<Transition
-      items={ index }
-      from={ {
-        position: "absolute",
-        transform: "translate3d(0,-150px,0)",
-        opacity: 0
-      } }
-      enter={ { transform: "translate3d(0,0px,0)", opacity: 1 } }
-      leave={ { transform: "translate3d(0,150px,0)", opacity: 0 } }
-      config={ {
-        mass: 10,
-        friction: 75
-      } }
-    >
-      { newIndex => props => {
-        const newJob = jobs[ newIndex ];
-        return (
-          <div style={ props }>
-            <h3>{ newJob.title }</h3>
-            <List>
-              { newJob.description.map((item, i) => (
-                <List.Item key={ i }>{ item }</List.Item>
-              )) }
-            </List>
-          </div>
-        );
-      } }
-    </Transition>
-    )
-
-  }
+  renderDescription = index => {
+    const offset = -150;
+    return (
+      <div style={{ postiion: "relative" }}>
+        <Transition
+          items={index}
+          style={{ position: "relative" }}
+          from={{
+            position: "absolute",
+            transform: `translate3d(0,${offset}px,0)`,
+            opacity: 0
+          }}
+          enter={{ transform: "translate3d(0,0px,0)", opacity: 1 }}
+          leave={{ transform: `translate3d(0,${offset * -1}px,0)`, opacity: 0 }}
+          config={{
+            mass: 10,
+            friction: 75
+          }}
+        >
+          {newIndex => props => {
+            const newJob = jobs[newIndex];
+            return (
+              <div style={props}>
+                <h2 className="trails-header-2">{newJob.title}</h2>
+                <ListGroup variant="flush">
+                  {newJob.description.map((item, i) => (
+                    <ListGroupItem className="trails-menu-text" key={i}>
+                      {item}
+                    </ListGroupItem>
+                  ))}
+                </ListGroup>
+              </div>
+            );
+          }}
+        </Transition>
+      </div>
+    );
+  };
 
   renderGrid = () => {
     const { index } = this.state;
     return (
-      <Container className="parallax-grid">
-        <Row className="justify-content-md-center">
-          <Col xs={ 12 } md={ 4 } >
-            { this.renderSelect(index) }
+      <Container className="parallax-grid" style={{ height: "25vh" }}>
+        <Row className="justify-content-center">
+          <Col xs={12} lg={4} className="justify-content-center">
+            {this.renderSelect(index)}
           </Col>
-          <Col xs={ 12 } md={ 4 } >
-            { this.renderDescription(index) }
+          <Col xs={12} lg={6}>
+            {this.renderDescription(index)}
           </Col>
         </Row>
-      </Container >
-    )
-  }
+      </Container>
+    );
+  };
 
   render() {
     return this.renderGrid();
