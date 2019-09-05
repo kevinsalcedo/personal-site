5ba252f7ae87c997bb71dafe74cd1041769a5285
Kevin Salcedo <kevinsalcedo@gmail.com>
2019-09-05 16:28:16 -0500

    filled out the page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,39 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import { about } from "../data/about";
 
 const About = () => {
   return (
-    <div>
-      <h1>It me</h1>
-    </div>
+    <Container className="parallax-grid">
+      <Row>
+        <Col xs={12} md={8}>
+          {about["body"].map((item, i) => (
+            <p className="trails-body" key={i}>
+              {item}
+            </p>
+          ))}
+        </Col>
+        <Col
+          md={4}
+          className="justify-content-middle"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <Image
+            className="mah-face"
+            src={about["photo"]}
+            alt="me"
+            roundedCircle
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
