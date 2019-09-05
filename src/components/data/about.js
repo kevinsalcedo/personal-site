b636524595d959b07bec31d8957fcceda3933888
Kevin Salcedo <kevinsalcedo@gmail.com>
2019-09-05 16:27:49 -0500

    edited body text to fix grammar and spelling error

--- src/components/data/about.js
+++ src/components/data/about.js
@@ -1,5 +1,10 @@
+import me from "../../assets/it-me.jpg";
+
 export const about = {
   title: "About Me",
-  body:
-    "My name is Kevin Salcedo, and I'm a full stack software engineer based out of beautiful Austin, Texas. I strive to create functional and effient code that is easy to use and a pleasure to see.\n "
+  body: [
+    "My name is Kevin Salcedo, a full stack software engineer based out of beautiful Austin, Texas. I strive to create functional and efficient code that translates into a smooth user experience.",
+    "I'm currently employed at Conduent, helping migrate our disease surveillance application to utilize the React library."
+  ],
+  photo: me
 };
