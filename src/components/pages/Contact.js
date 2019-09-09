e36b7c2613c5cf3863f3e73025f188126f3fe881
Kevin Salcedo <kevinsalcedo@gmail.com>
2019-09-09 10:06:40 -0500

    added Contact template

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Contact = () => {
+  return (
+    <Container className="parallax-grid">
+      <Row className="justify-content-center flex-column">
+        <Col className="flex-column justify-content-center">
+          <p className="trails-body">Want to get in touch?</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Contact;
